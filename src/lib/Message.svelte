a83c443ed7af2abb21054ecb44b3a2022641e613
Hein Lin Aung <heinlinaung6891@gmail.com>
2023-02-06 18:37:14 +0630

    Updated : Message.svelte [add/remove chat realtime]

--- src/lib/Message.svelte
+++ src/lib/Message.svelte
@@ -3,14 +3,42 @@
   import { currentUser, pb } from "./pocketbase";
 
   let messages = [];
-
+  let newMessage: string;
+  let unsubscribe: () => void;
   onMount(async () => {
     const resultList = await pb.collection("messages").getList(1, 50, {
       sort: "created",
       expand: "user",
     });
     messages = resultList.items;
+
+    // subscribe to realtime message
+    unsubscribe = await pb
+      .collection("messages")
+      .subscribe("*", async ({ action, record }) => {
+        if (action === "create") {
+          // Fetch associated user
+          const user = await pb.collection("users").getOne(record.user);
+          record.expand = { user };
+          messages = [...messages, record];
+        }
+        if (action === "delete") {
+          messages = messages.filter((m) => m.id !== record.id);
+        }
+      });
   });
+
+  onDestroy(() => {
+    unsubscribe();
+  });
+
+  async function sendMessage() {
+    const data = {
+      text: newMessage,
+      user: $currentUser.id,
+    };
+    const createdMessage = await pb.collection("messages").create(data);
+  }
 </script>
 
 <div class="messages">
@@ -31,3 +59,8 @@
     </div>
   {/each}
 </div>
+
+<form on:submit|preventDefault={sendMessage}>
+  <input type="text" placeholder="Message" bind:value={newMessage} />
+  <button type="submit">Send it!</button>
+</form>
